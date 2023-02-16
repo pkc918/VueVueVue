e5eb9a87832d40b483907c0084b4ca0af8196440
XiaoPan <2489964425@qq.com>
2023-02-16 11:40:43 +0800

    add comment

--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -3,6 +3,7 @@ describe("effect", () => {
     // initialization
     const user = reactive({ age: 22 });
     let newAge;
+    // 依赖函数，在get时，收集，set时触发
     effect(() => {
       newAge = user.age + 1;
     });
