3bc8f38a0cdfa2986631dcdd094082c7b9d01996
XiaoPan <2489964425@qq.com>
2023-02-16 11:42:43 +0800

    a functional test of one the Reactivi

---
+++ src/reactivity/tests/reactive.spec.ts
@@ -0,0 +1,9 @@
+describe("reactive", () => {
+  it("happy patch", () => {
+    const origin = { foo: 1 };
+    const instance_reactive = reactive(origin);
+    // 对象之间引用不相等
+    expect(instance_reactive).not.toBe(origin);
+    expect(instance_reactive.foo).toBe(1);
+  });
+});
