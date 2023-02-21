6e4b7be91691aab278e99fda2aa5e50a945706c7
XiaoPan <2489964425@qq.com>
2023-02-21 10:14:38 +0800

    scheduler test

--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -27,4 +27,32 @@ describe("effect", () => {
     const runner_return = runner();
     expect(runner_return).toBe("foo");
   });
+
+  it("scheduler", () => {
+    let dummy;
+    let run: any;
+    const scheduler = jest.fn(() => {
+      run = runner;
+    });
+    const obj = reactive({ foo: 1 });
+    /* 
+      effect 第一次执行，执行fn
+      响应式对象 set update 的时候，执行 scheduler
+      当执行 runner 的时候，会再次调用 fn
+    */
+    const runner = effect(
+      () => {
+        dummy = obj.foo;
+      },
+      { scheduler }
+    );
+    // 不会被调用
+    expect(scheduler).not.toHaveBeenCalled();
+    expect(dummy).toBe(1);
+    obj.foo++;
+    expect(scheduler).toHaveBeenCalledTimes(1);
+    expect(dummy).toBe(1);
+    run();
+    expect(dummy).toBe(2);
+  });
 });
