a9670eba7a0357aceaf61e88173e3ffe0c9582af
XiaoPan <2489964425@qq.com>
2023-02-22 16:46:51 +0800

    isReactive function

--- src/reactivity/baseHandlers.ts
+++ src/reactivity/baseHandlers.ts
@@ -1,4 +1,5 @@
 import { track, trigger } from "./effect";
+import { ReactiveFlags } from "./reactive";
 // 只在初始化的时候创建一次
 const get = createGetter();
 const set = createSetter();
@@ -7,6 +8,11 @@ const readonlyGet = createGetter(true);
 function createGetter(isReadonly = false) {
   return function get(target, key) {
     const res = Reflect.get(target, key);
+    if (key === ReactiveFlags.IS_REACTIVE) {
+      return !isReadonly;
+    } else if (key === ReactiveFlags.IS_READONLY) {
+      return isReadonly;
+    }
     if (!isReadonly) {
       // 依赖收集
       track(target, key);
--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,5 +1,10 @@
 import { mutableHandlers, readonlyHandlers } from "./baseHandlers";
 
+export const enum ReactiveFlags {
+  IS_REACTIVE = "__v_isReactive",
+  IS_READONLY = "__v_isReadonly",
+}
+
 function createActivityObject(raw, baseHandlers) {
   return new Proxy(raw, baseHandlers);
 }
@@ -10,4 +15,9 @@ export function reactive(raw) {
 
 export function readonly(raw) {
   return createActivityObject(raw, readonlyHandlers);
+}
+
+export function isReactive(value) {
+  // 设置属性，触发get函数，当不是Proxy对象时，会直接 undefined，不会触发 get，需要转换boolean
+  return !!value[ReactiveFlags.IS_REACTIVE];
 }
