fe37600b07cf9e8fac23d2c9b41833d5a2785201
XiaoPan <2489964425@qq.com>
2023-02-22 14:03:24 +0800

    readonly function

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,18 +1,37 @@
 import { track, trigger } from "./effect";
 
-export function reactive(raw) {
-  return new Proxy(raw, {
-    get(target, key) {
-      const res = Reflect.get(target, key);
+function createGetter(isReadonly: boolean = false) {
+  return function get(target, key) {
+    const res = Reflect.get(target, key);
+    if (!isReadonly) {
       // 依赖收集
       track(target, key);
-      return res;
-    },
+    }
+    return res;
+  };
+}
+
+function createSetter() {
+  return function set(target, key, value) {
+    const res: boolean = Reflect.set(target, key, value);
+    // 触发依赖
+    trigger(target, key);
+    return res;
+  };
+}
+
+export function reactive(raw) {
+  return new Proxy(raw, {
+    get: createGetter(),
+    set: createSetter(),
+  });
+}
+
+export function readonly(raw) {
+  return new Proxy(raw, {
+    get: createGetter(true),
     set(target, key, value) {
-      const res: boolean = Reflect.set(target, key, value);
-      // 触发依赖
-      trigger(target, key);
-      return res;
+      return true;
     },
   });
 }
