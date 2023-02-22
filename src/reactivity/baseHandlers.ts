11105942ebc958cb8b646ede71d556f118eab20b
XiaoPan <2489964425@qq.com>
2023-02-22 16:02:43 +0800

    encapsulation function

---
+++ src/reactivity/baseHandlers.ts
@@ -0,0 +1,37 @@
+import { track, trigger } from "./effect";
+// 只在初始化的时候创建一次
+const get = createGetter();
+const set = createSetter();
+const readonlyGet = createGetter(true);
+
+function createGetter(isReadonly = false) {
+  return function get(target, key) {
+    const res = Reflect.get(target, key);
+    if (!isReadonly) {
+      // 依赖收集
+      track(target, key);
+    }
+    return res;
+  };
+}
+
+function createSetter() {
+  return function set(target, key, value) {
+    const res: boolean = Reflect.set(target, key, value);
+    // 触发依赖
+    trigger(target, key);
+    return res;
+  };
+}
+
+export const mutableHandlers = {
+  get,
+  set,
+};
+
+export const readonlyHandlers = {
+  get: readonlyGet,
+  set(target, key, value) {
+    return true;
+  },
+};
--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,37 +1,13 @@
-import { track, trigger } from "./effect";
+import { mutableHandlers, readonlyHandlers } from "./baseHandlers";
 
-function createGetter(isReadonly: boolean = false) {
-  return function get(target, key) {
-    const res = Reflect.get(target, key);
-    if (!isReadonly) {
-      // 依赖收集
-      track(target, key);
-    }
-    return res;
-  };
-}
-
-function createSetter() {
-  return function set(target, key, value) {
-    const res: boolean = Reflect.set(target, key, value);
-    // 触发依赖
-    trigger(target, key);
-    return res;
-  };
+function createActivityObject(raw, baseHandlers) {
+  return new Proxy(raw, baseHandlers);
 }
 
 export function reactive(raw) {
-  return new Proxy(raw, {
-    get: createGetter(),
-    set: createSetter(),
-  });
+  return createActivityObject(raw, mutableHandlers);
 }
 
 export function readonly(raw) {
-  return new Proxy(raw, {
-    get: createGetter(true),
-    set(target, key, value) {
-      return true;
-    },
-  });
+  return createActivityObject(raw, readonlyHandlers);
 }
