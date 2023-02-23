5bc4e4d9c8e980a44aca5d28af736823528361be
XiaoPan <2489964425@qq.com>
2023-02-23 17:42:25 +0800

    make nested properties reactive in Ref

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -1,11 +1,15 @@
 import { trackEffect, triggerEffects, isTracking } from "./effect";
-import { hasChanged } from "./shared";
+import { reactive } from "./reactive";
+import { hasChanged, isObject } from "./shared";
 
 class RefImpl {
   private _value: any;
   public dep;
+  // 存储未被处理过的 value 值，应为需要对比，如果多层对象，修改的时候，是一个对象和Proxy对象比较，所以有问题
+  private _rawValue: any;
   constructor(value) {
-    this._value = value;
+    this._rawValue = value;
+    this._value = covert(value);
     this.dep = new Set();
   }
 
@@ -16,12 +20,18 @@ class RefImpl {
 
   set value(newValue) {
     // 前后值发生了改变，就通知
-    if (hasChanged(newValue, this._value)) {
-      // 先修改 value 值，再通知
-      this._value = newValue;
+    if (hasChanged(newValue, this._rawValue)) {
+      // 当前值记录
+      this._rawValue = newValue;
+      // 先修改 value 值，再通知, 讲直接赋的值变为响应式
+      this._value = covert(newValue);
       triggerEffects(this.dep);
     }
   }
+}
+
+function covert(value) {
+  return isObject(value) ? reactive(value) : value;
 }
 
 function trackRefValue(ref) {
