959a0d45da1d0868dc52800280b70b57735b6406
XiaoPan <2489964425@qq.com>
2023-03-07 12:44:00 +0800

    实现注册事件功能案例

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -11,6 +11,12 @@ export const App = {
       {
         id: "root",
         class: ["red", "blue"],
+        onClick() {
+          console.log("click");
+        },
+        onMousedown() {
+          console.log("mousedown");
+        },
       },
       "hi " + this.msg
       // [h("p", { class: "red" }, "hi"), h("p", { class: "blue" }, "mini-vue")]
