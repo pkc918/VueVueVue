bc870ebed3602be746350f1f592a7e204ecd73e5
XiaoPan <2489964425@qq.com>
2023-03-15 00:07:30 +0800

    provide & inject 案例

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -3,9 +3,63 @@ import {
   h,
   createTextVNode,
   getCurrentInstance,
+  provide,
+  inject,
 } from "../../lib/guide-vue_vue_vue.esm.js";
 
 window.self = null;
+const Provider = {
+  name: "Provider",
+  setup() {
+    provide("foo", "fooVal");
+    provide("bar", "barVal");
+  },
+  render() {
+    return h("div", {}, [h("p", {}, "Provider"), h(ProviderTwo)]);
+  },
+};
+
+const ProviderTwo = {
+  name: "ProviderTwo",
+  setup() {
+    provide("foo", "fooTwo");
+    // provide("bar", "barVal");
+    const foo = inject("foo");
+    return {
+      foo,
+    };
+  },
+  render() {
+    return h("div", {}, [
+      h("p", {}, `ProviderTwo: - ${this.foo}`),
+      h(Consumer),
+    ]);
+  },
+};
+
+const Consumer = {
+  name: "Consumer",
+  setup() {
+    const foo = inject("foo");
+    const bar = inject("bar");
+    const baz = inject("baz", "bazDefault");
+    const baz1 = inject("baz1", () => "baz11111");
+    return {
+      foo,
+      bar,
+      baz,
+      baz1,
+    };
+  },
+  render() {
+    return h(
+      "div",
+      {},
+      `Consumer: - ${this.foo} - ${this.bar} - ${this.baz} - ${this.baz1}`
+    );
+  },
+};
+
 export const App = {
   name: "App",
   render() {
@@ -26,6 +80,7 @@ export const App = {
         },
       },
       [
+        h(Provider, {}),
         h("div", {}, "hi, " + this.msg),
         // 组件下的 children 就是 slots
         h(
