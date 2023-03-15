18bd0dcebabf3b311ddb8baa47a42c7d105c9b6d
XiaoPan <2489964425@qq.com>
2023-03-15 21:02:40 +0800

    清除 log

--- src/runtime-dom/index.ts
+++ src/runtime-dom/index.ts
@@ -2,13 +2,9 @@ import { createRenderer } from "../runtime-core";
 import { isOnEventName } from "../shared";
 
 function createElement(type) {
-  console.log("createElement---------");
-
   return document.createElement(type);
 }
 function patchProp(el, key, val) {
-  console.log("patchProp---------");
-
   if (isOnEventName(key)) {
     const eventName = key.slice(2).toLowerCase();
     el.addEventListener(eventName, val);
